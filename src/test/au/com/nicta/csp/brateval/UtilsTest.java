c79377f428f2618d2b510449128e4ce8b8a8eb43
Ameer Albahem <ameer.albahem@gmail.com>
2020-01-29 16:51:27 +1100

    Adding UtilsTest

---
+++ src/test/au/com/nicta/csp/brateval/UtilsTest.java
@@ -0,0 +1,14 @@
+package au.com.nicta.csp.brateval;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @org.junit.jupiter.api.Test
+    void mapValueIntegerSortDesc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plusMap() {
+    }
+}
