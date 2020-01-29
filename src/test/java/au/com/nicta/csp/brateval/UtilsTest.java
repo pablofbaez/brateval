8e829eff1be5f672f104135b6184d1e293e92603
Ameer Albahem <ameer.albahem@gmail.com>
2020-01-29 17:10:07 +1100

    Adding Junit4 dependency

---
+++ src/test/java/au/com/nicta/csp/brateval/UtilsTest.java
@@ -0,0 +1,14 @@
+package au.com.nicta.csp.brateval;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+
+    @org.junit.Test
+    public void mapValueIntegerSortDesc() {
+    }
+
+    @org.junit.Test
+    public void plusMap() {
+    }
+}
